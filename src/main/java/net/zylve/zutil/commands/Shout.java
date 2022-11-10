7cb4a437e62c74da3f058906e813c09fcc70233b
Zylve <70604613+Zylve@users.noreply.github.com>
2022-11-09 18:38:39 -0800

    Recude clutter in shout

--- src/main/java/net/zylve/zutil/commands/Shout.java
+++ src/main/java/net/zylve/zutil/commands/Shout.java
@@ -1,11 +1,12 @@
 package net.zylve.zutil.commands;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class Shout implements CommandExecutor {
     @Override
@@ -16,13 +17,17 @@ public class Shout implements CommandExecutor {
 
         String world = "";
 
+        int x = (int)player.getLocation().getX();
+        int y = (int)player.getLocation().getY();
+        int z = (int)player.getLocation().getZ();
+
         switch(player.getWorld().getName()) {
             case "world":
-                world = String.format("the Overworld (%d %d %d Nether)", (int)player.getLocation().getX() / 8, (int)player.getLocation().getY(), (int)player.getLocation().getZ() / 8);
+                world = String.format("the Overworld (%d %d %d Nether)", x / 8, y, z / 8);
                 break;
 
             case "world_nether":
-            world = String.format("the Nether (%d %d %d Overworld)", (int)player.getLocation().getX() * 8, (int)player.getLocation().getY(), (int)player.getLocation().getZ() * 8);
+            world = String.format("the Nether (%d %d %d Overworld)", x * 8, y, z * 8);
                 break;
 
             case "world_the_end":
@@ -34,7 +39,7 @@ public class Shout implements CommandExecutor {
                 break;
         }
 
-        String message = String.format("%s is at %d %d %d in %s", player.getName(), (int)player.getLocation().getX(), (int)player.getLocation().getY(), (int)player.getLocation().getZ(), world);
+        String message = String.format("%s is at %d %d %d in %s", player.getName(), x, y, z, world);
 
         Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + message);
         return true;
