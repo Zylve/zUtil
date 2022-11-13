5583447374d540aee955d8c938587bb4f16deb4f
Zylve <70604613+Zylve@users.noreply.github.com>
2022-11-12 18:22:36 -0800

    Test

--- src/main/java/net/zylve/zutil/events/DeathLocation.java
+++ src/main/java/net/zylve/zutil/events/DeathLocation.java
@@ -14,26 +14,26 @@ public class DeathLocation implements Listener {
         int y = (int)location.getY();
         int z = (int)location.getZ();
 
-        String append = "";
+        String message = event.getDeathMessage();
 
         switch(location.getWorld().getName()) {
             case "world":
-                append = String.format(" at %d %d %d in the Overworld", x, y, z);
+                message += String.format(" at %d %d %d in the Overworld", x, y, z);
                 break;
 
             case "world_nether":
-                append = String.format(" at %d %d %d in the Nether", x, y, z);
+                message += String.format(" at %d %d %d in the Nether", x, y, z);
                 break;
 
             case "world_the_end":
-                append = String.format(" at %d %d %d in The End", x, y, z);
+                message += String.format(" at %d %d %d in The End", x, y, z);
                 break;
 
             default:
-                append = String.format(" at %d %d %d in an unknown dimension", x, y, z);
+                message += String.format(" at %d %d %d in an unknown dimension", x, y, z);
                 break;
         }
 
-        event.setDeathMessage(event.getDeathMessage() + append);
+        event.setDeathMessage(message);
     }
 }
